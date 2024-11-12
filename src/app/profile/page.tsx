f91cba3e54d5dc6bbd66724c817a64ba65b7b0b5
Asham0711 <mdashamimad3246@gmail.com>
2024-11-12 15:17:04 +0530

    Added full frontend and middleware. Thus completed the project

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import React, { useState } from 'react'
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import {toast} from "react-hot-toast";
 
-const page = () => {
+const ProfilePage = () => {
+  const router = useRouter();
+  const [data, setData] = useState("");
+  
+  const getUserDetails = async () =>{
+      try {
+          const response = await axios.post('/api/users/me');
+          console.log(response.data);
+          setData(response.data.data._id)
+      } catch (error:any) {
+        console.log("Error while fetching data");
+        toast.error(error.message);
+      }
+  }
+
+  const logout = async () => {
+      try {
+        await axios.get('/api/users/logout');
+        toast.success("User logged out successfully");
+        router.push('/login');
+      } catch (error:any) {
+        console.log("Error while logging out");
+        toast.error(error.message);
+      }
+  }
+
   return (
-    <div>Profile page</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === '' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}
+      </h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
+      </button>
+    </div>
   )
 }
 
-export default page
+export default ProfilePage
